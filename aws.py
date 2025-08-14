5fd60604682fb0af98d1e2b6219649e8aab24e36
DongGwan0505 <dong123649@gmail.com>
2025-08-14 14:52:35 +0900

    "배우 찾기"에서 인물 사진 크롤링 기능 추가

--- aws.py
+++ aws.py
@@ -1,4 +1,48 @@
 import boto3
+import requests
+
+def get_wikidata_image(wikidata_id):
+    """
+    Wikidata ID를 이용해 인물 이미지 URL을 가져오는 함수
+    """
+    if not wikidata_id:
+        return None
+    
+    # Wikidata API 엔드포인트
+    url = "https://www.wikidata.org/w/api.php"
+    
+    # API 요청 파라미터
+    params = {
+        "action": "wbgetentities",
+        "ids": wikidata_id,
+        "props": "claims",
+        "format": "json",
+    }
+    
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        
+        # P18 (image) 속성에서 이미지 파일 이름을 추출
+        claims = data['entities'][wikidata_id]['claims']
+        if 'P18' in claims:
+            # 첫 번째 이미지 파일 이름 가져오기
+            image_filename = claims['P18'][0]['mainsnak']['datavalue']['value']
+            
+            # 파일 이름을 이용해 Commons Media 이미지 URL 생성
+            # 이미지 파일 이름은 공백을 _로 바꿔줘야 합니다.
+            image_filename = image_filename.replace(' ', '_')
+            
+            # Commons Media 이미지 URL
+            # md5 해시를 이용한 이미지 경로 생성
+            import hashlib
+            md5_hash = hashlib.md5(image_filename.encode('utf-8')).hexdigest()
+            
+            return f"https://upload.wikimedia.org/wikipedia/commons/thumb/{md5_hash[0]}/{md5_hash[0:2]}/{image_filename}/200px-{image_filename}"
+            
+    except Exception as e:
+        print(f"Error fetching image from Wikidata: {e}")
+        return None
 
 def recognize_celebrities(photo):
     client = boto3.client('rekognition')
@@ -16,11 +60,25 @@ def recognize_celebrities(photo):
     # 감지된 유명인 정보 리스트
     celebrity_list = []
     for celebrity in response['CelebrityFaces']:
-        celebrity_info = {
+        celeb_info = {
             "name": celebrity.get('Name', '알 수 없음'),
             "gender": celebrity.get('KnownGender', {}).get('Type', '알 수 없음'),
             "urls": celebrity.get('Urls', [])
         }
-        celebrity_list.append(celebrity_info)
+        # Wikidata URL에서 ID를 추출하여 이미지 검색
+        wikidata_id = None
+        for url in celeb_info['urls']:
+            if "wikidata.org/wiki/" in url:
+                wikidata_id = url.split('/')[-1]
+                break
+        
+        # Wikidata API를 호출해 이미지 URL 가져오기
+        if wikidata_id:
+            image_url = get_wikidata_image(wikidata_id)
+            celeb_info['image_url'] = image_url
+        else:
+            celeb_info['image_url'] = None
+        
+        celebrity_list.append(celeb_info)
         
     return {"result": "success", "celebrities": celebrity_list}
